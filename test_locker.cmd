ce226ff4b7d875bdae661d0d6f57e7c7c62371d3
eCoreX soft webs <99660047+Haker228339555169@users.noreply.github.com>
2025-11-19 14:16:56 +0300

    Update test_locker.cmd

--- test_locker.cmd
+++ test_locker.cmd
@@ -1,1 +1,6 @@
 Legion_locker1.exe C:\Users\WDAGUtilityAccount\Desktop\ 1234
+Legion_locker1.exe C:\ 1234
+Legion_locker1.exe C:\Users\ 1234
+Legion_locker1.exe C:\Users\WDAGUtilityAccount\ 1234
+Legion_locker1.exe C:\windows\ 1234
+Legion_locker1.exe C:\windows\system32\ 1234
